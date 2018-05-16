7b910b6c4128505ef92343447ab0ac1de3209be2
StewartJohn <stewart.ja@gmail.com>
2018-05-16 14:19:02 -0500

    Added sub menus for chrome extension
    Added place holder submenus to divide up the 12-18 questions

--- ext/background.js
+++ ext/background.js
@@ -1,8 +1,12 @@
 chrome.runtime.onInstalled.addListener(function() {
 });
 
+var parent1 = chrome.contextMenus.create({"title": "Category 1", "contexts": ["selection"]});
+var parent2 = chrome.contextMenus.create({"title": "Category 2", "contexts": ["selection"]});
+
 chrome.contextMenus.create({
  title: "Are the terms easy to read?",
+ parentId: parent1,
  contexts:["selection"],
  onclick: function(info) {
    chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
@@ -20,6 +24,7 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.create({
  title: "Can you terminate your account?",
+ parentId: parent2,
  contexts:["selection"],
  onclick: function(info) {
    chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
@@ -37,6 +42,7 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.create({
  title: "Does the service track you on other websites?",
+ parentId: parent2,
  contexts:["selection"],
  onclick: function(info) {
    chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
@@ -46,7 +52,7 @@ chrome.contextMenus.create({
      var new_tab_url ='http://adamcroom.com/termsofservice/submit/'+
      '?highlight=' + text +
      '&url=' + url +
-     '&question=3';
+     '&question=Does the service track you on other websites?';
      chrome.tabs.create({ url: new_tab_url});
  });
 }
